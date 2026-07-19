efa17b97f1287222352e4076f8d8378e948ad056
TTHookGen <urbanlegends765@gmail.com>
2026-07-19 01:11:45 -0400

    Fix blank screen and update routing for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Most reliable for Capacitor local file loading
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
