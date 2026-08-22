23306948deb71ac6593c74888cbe2dc88f72d231
TTHookGen <urbanlegends765@gmail.com>
2026-08-22 03:52:13 -0400

    replaced icon and splash

--- android/app/src/main/java/fun/mybingox/MainActivity.java
+++ android/app/src/main/java/fun/mybingox/MainActivity.java
@@ -1,6 +1,7 @@
 package fun.mybingox;
 
 import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 import io.capawesome.capacitorjs.plugins.androidedgetoedgesupport.EdgeToEdgePlugin;
 import fun.mybingox.unity.UnityAdsPlugin;
@@ -8,6 +9,7 @@ import fun.mybingox.unity.UnityAdsPlugin;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         registerPlugin(EdgeToEdgePlugin.class);
         registerPlugin(UnityAdsPlugin.class);
         super.onCreate(savedInstanceState);
